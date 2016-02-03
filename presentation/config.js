27283a57ba636c6a094a6f23cdf19601f3b6563d
Mark Redeman <markredeman@gmail.com>
2016-02-03 21:36:19 +0100

    Remove all inpreperation of upgrade

--- presentation/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  width: 1000,
-  height: 900,
-  margin: 40,
-  theme: require("../themes/custom/index"),
-  print: require("../themes/custom/print"),
-  html: require("../themes/custom/html")
-};
